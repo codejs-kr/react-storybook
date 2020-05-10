adb9c3df05329b668bbf7cbce30f262cf8ea3205
dodortus <dodortus@gmail.com>
2020-05-10 19:09:22 +0900

    Update styled comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
+import Button from 'components/Button';
+import Dialog from 'components/Dialog';
 import 'assets/style/main.scss';
 
-class App extends Component {
-  render() {
-    return <Fragment>APP</Fragment>;
-  }
-}
+const App = () => {
+  return (
+    <Fragment>
+      <div>
+        <Button>버튼</Button>
+      </div>
+
+      <div>
+        <Dialog type="default" title="Title">
+          <main>내용입니다</main>
+          <footer>
+            <Button size="normal" color="sub">
+              취소
+            </Button>
+            <Button size="normal" color="primary">
+              확인
+            </Button>
+          </footer>
+        </Dialog>
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import './Button.scss';
+// import './Button.scss';
+import styled from 'styled-components';
+
+const ButtonSC = styled.button`
+  margin: 10px;
+  padding: 0 100px;
+  line-height: 1;
+  min-width: 100px;
+  height: 60px;
+  color: #fff;
+  background-color: #78cd13;
+  border-radius: 4px;
+
+  // size
+  &.small {
+    padding: 0 74px;
+    height: 48px;
+    font-size: 1.4rem;
+  }
+
+  &.large {
+    padding: 0 183px;
+    height: 70px;
+    font-size: 1.8rem;
+  }
+
+  // color, type
+  &.sub {
+    background: #f1f3f5;
+    color: #343a40;
+  }
+
+  &.outline {
+    background: none;
+    color: #343a40;
+    border: solid 1px #dee2e6;
+  }
+`;
 
 const Button = ({ children = '버튼', type = 'button', size = 'normal', color = 'primary', onClick = () => {} }) => {
-  const className = `button ${size} ${color}`;
+  const className = `${size} ${color}`;
 
   return (
-    <button type={type} className={className} onClick={onClick}>
+    <ButtonSC type={type} className={className} onClick={onClick}>
       {children}
-    </button>
+    </ButtonSC>
   );
 };
 
