08425e40550b32aaf8aadf788d4704b59ee9ac6d
dodortus <dodortus@gmail.com>
2020-07-08 23:56:40 +0900

    feat: 스토리보드 이미지 alias 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../stories/**/*.stories.js', '../src/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-storysource'],
@@ -5,13 +7,14 @@ module.exports = {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
-
     // Make whatever fine-grained changes you need
     config.module.rules.push({
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
-      // include: path.resolve(__dirname, '../'),
     });
+
+    // Set style asset alias
+    config.resolve.modules.push(path.resolve(__dirname, '../src'));
 
     // Return the altered config
     return config;
